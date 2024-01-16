882cf5e61626db439b6a4427a68899cfa9befba8
Hendrik Neumann <henne@armselig.net>
2024-01-16 16:07:20 +0100

    chore: INIT

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
